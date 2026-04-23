debe0a221a159c71f1c0dacb2ed35cee7ae24bbe
lequochaidevh <lequochaidevh@gmail.com>
2026-04-23 20:47:56 +0700

    Added build option and clang-format

---
+++ testing_space/TestVer0_0_0/HarisTinyOS/source/common/pch/haris_build_option.h
@@ -0,0 +1,21 @@
+/**
+ ******************************************************************************
+ * @Author: HaiLQ
+ * @Date:   23/04/2026
+ ******************************************************************************
+ **/
+#ifndef __HARIS_BUILD_OPTION_H__
+#define __HARIS_BUILD_OPTION_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define __HARIS_PACKETED __attribute__((__packed__))
+#define __HARIS_WEAK     __attribute__((__weak__))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  //__HARIS_BUILD_OPTION_H__
