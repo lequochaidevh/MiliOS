0284e49ca94871d54b48dcadccd77a54c08fbef3
lequochaidevh <lequochaidevh@gmail.com>
2026-04-26 13:47:53 +0700

    Clean up and Restructure working dir

--- HarisTinyOS/build_and_run.sh
+++ HarisTinyOS/build_and_run.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-rm -rf x86_64_build && mkdir x86_64_build && cd x86_64_build && cmake ../source/ && make && clear && ./dummy_app
+PLATFORM_TARGET="x86_64"
+APP_NAME="dummy_app"
+CMAKE_TARGET_DIR="source/platform/${PLATFORM_TARGET}/${APP_NAME}/CMakeLists.txt"
+
+# Copy CMake compatiable with platform
+cp $CMAKE_TARGET_DIR source/
+
+BUILD_DIR="${PLATFORM_TARGET}_build_${APP_NAME}"
+
+# Clean sstate
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+
+# BUILD
+cd $BUILD_DIR
+cmake ../source/ 
+make
+
+# Clean screen
+clear
+
+# Run app
+./dummy_app
